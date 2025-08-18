6dd169052639e9cc5d9470785e075d4d1775269b
AB <b2ra830@gmail.com>
2025-08-18 19:28:55 +0530

    insertion sorted , merge and quick sort will be

--- 6. sortingJava/insertionSort.java
+++ 6. sortingJava/insertionSort.java
@@ -5,9 +5,9 @@ public class InsertionSort {
         int arr[] = {7, 8, 3, 1, 2};
 
         // insertion sort
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 1; i < arr.length; i++) {
             int current = arr[i];
-            int j = i - 1; //back element (phle ka) 
+            int j = i - 1; // back sorted last element (for compare) 
             while (j >= 0 && current < arr[j]) {
                 arr[j + 1] = arr[j];
                 j--; //decrease pos
