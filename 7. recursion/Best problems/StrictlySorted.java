11c9e81d84e18bf2d381c4039cf205445c2359d7
AB <b2ra830@gmail.com>
2025-08-20 11:40:53 +0530

    strictly sorted array in recursion

---
+++ 7. recursion/Best problems/StrictlySorted.java
@@ -0,0 +1,20 @@
+
+class StrictlySorted {
+
+    // strictly sorted : not equals any number
+    private static boolean isSorted(int arr[], int indx) {
+        if (indx == arr.length - 1) {
+            return true;
+        }
+        if (arr[indx] < arr[indx + 1]) {
+            return isSorted(arr, indx + 1);
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        int array[] = {25, 28, 34, 54, 56};
+        System.out.println("Result is : " + isSorted(array, 0));
+    }
+}
