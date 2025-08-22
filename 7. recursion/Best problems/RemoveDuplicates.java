a691963ff6fdf9a136eb1461276f2c05c4431067
AB <b2ra830@gmail.com>
2025-08-22 09:56:07 +0530

    removeDuplicates modified

--- 7. recursion/Best problems/RemoveDuplicates.java
+++ 7. recursion/Best problems/RemoveDuplicates.java
@@ -1,6 +1,7 @@
 
 public class RemoveDuplicates {
 
+    // store true or false in 0 - 25 indices of array 
     private static boolean[] AtoZ = new boolean[26];
 
     private static void removeDuplicates(String str, int indx, String newString) {
@@ -9,9 +10,11 @@ public class RemoveDuplicates {
             return;
         }
         // 'b' - 'a' = 1
+        // if true then came second or more times so don't add into newString 
         if (AtoZ[str.charAt(indx) - 'a']) {
             removeDuplicates(str, indx + 1, newString);
         } else {
+            // came first time so add into newString and store true in that indx of array
             newString += str.charAt(indx);
             AtoZ[str.charAt(indx) - 'a'] = true;
             removeDuplicates(str, indx + 1, newString);
