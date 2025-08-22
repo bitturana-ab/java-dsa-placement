f6c49e51cb369562729dc366bf3c535179da27d7
AB <b2ra830@gmail.com>
2025-08-22 09:38:39 +0530

    understood towerofhanoi alg

--- 7. recursion/Best problems/TowerOfHanoi.java
+++ 7. recursion/Best problems/TowerOfHanoi.java
@@ -2,12 +2,15 @@
 public class TowerOfHanoi {
 
     private static void towerOfHanoi(int n, String src, String helper, String dest) {
+        // transfer last one disk, src to dest direct
         if (n <= 1) {
             System.out.println("Transfer disk " + n + " from " + src + " to " + dest);
             return;
         }
+        // n-1 disks transfer from src to helper using dest as a helper
         towerOfHanoi(n - 1, src, dest, helper);
         System.out.println("Transfer disk " + n + " from " + src + " to " + dest);
+        // again n-1 disks transfer from helper to dest using src as helper.
         towerOfHanoi(n - 1, helper, src, dest);
     }
 
