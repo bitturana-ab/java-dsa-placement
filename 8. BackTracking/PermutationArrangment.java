54f3e960f16d213ad7c3818d186e84a460dfaa9a
AB <b2ra830@gmail.com>
2025-08-27 23:40:39 +0530

    backtraking starting

--- 8. BackTracking/PermutationArrangment.java
+++ 8. BackTracking/PermutationArrangment.java
@@ -1,7 +1,7 @@
 
 public class PermutationArrangment {
 
-    private static void printArrangment(String str, String permu, int indx) {
+    private static void printArrangment(String str, String permu) {
         if (str.length() == 0) {
             System.out.println(permu);
             return;
@@ -9,12 +9,12 @@ public class PermutationArrangment {
         for (int i = 0; i < str.length(); i++) {
             char currChar = str.charAt(i); // 'A'
             String newStr = str.substring(0, i) + str.substring(i + 1); // "BC"
-            printArrangment(newStr, permu + currChar, indx + 1);
+            printArrangment(newStr, permu + currChar);
         }
     }
 
     public static void main(String[] args) {
         String str = "ABC";
-        printArrangment(str, "", 0);
+        printArrangment(str, "");
     }
 }
