cab4047432b4c00836678a0e6b7734a2164174dc
AB <b2ra830@gmail.com>
2025-08-31 16:29:27 +0530

    changing little bit things

--- 4. stringJavaCode/appendLast.java
+++ 4. stringJavaCode/appendLast.java
@@ -2,15 +2,16 @@
 class appendLast {
 
     public static void main(String args[]) {
-        // Strings are immutable in java(can't be changed/modified anywhere) but StringBuilder class can 
+        // Strings are immutable in java(can't be changed/modified anywhere) 
+        // but StringBuilder class can 
 
-        StringBuilder sb = new StringBuilder("B");
+        StringBuilder sb = new StringBuilder("S");
         // append (str = str + "i";) String la romba time n memory waste paninga eppo StringBuilder la append try pna
-        sb.append("i");
-        sb.append("t");
-        sb.append("t");
-        sb.append("u");
+        sb.append("a");
+        sb.append("n");
+        sb.append("j");
+        sb.append("a");
+        sb.append("y");
         System.out.println(sb);
-        System.out.println(sb.length());
     }
 }
--- 5.1 bitManipulation/GetBit.java
+++ 5.1 bitManipulation/GetBit.java
@@ -3,7 +3,7 @@ class GetBit {
 
     public static void main(String[] args) {
         int n = 5; //bit 0101 //knew 3rd bit 0 or 1s
-        int pos = 3; //2nd pos min 3 rd bit
+        int pos = 3; //2nd pos means 3 rd bit
         int bitWise = 1 << pos - 1; // for AND operation (0100)
 
         // 0100 & 0101 -> 0100 non zero number
