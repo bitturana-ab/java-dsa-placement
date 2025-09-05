5f2e5fbb7fd8fcee7126c8216f74cd29f7f9b6a9
bitturana-ab <b2ra830@gmail.com>
2025-09-05 22:00:32 +0530

    constructor and then parameterized contructor

--- 10. OOPS/ClassNObjects.java
+++ 10. OOPS/ClassNObjects.java
@@ -7,15 +7,21 @@ class Mobile {
     public void printInfo() {
         System.out.println(name + " " + price);
     }
+
+    public Mobile() {
+        System.out.println("constructor called");
+    }
+
 }
 
 public class ClassNObjects {
 
     public static void main(String jd[]) {
+        // Mobile m = new Mobile();
+        // m.name = "Samsung a54";
+        // m.price = 45000;
+        // m.printInfo();
         Mobile m = new Mobile();
-        m.name = "Samsung a54";
-        m.price = 45000;
-        m.printInfo();
 
     }
 }
