37829315666a652abc079d4676e4ff9ae8de3f91
bitturana-ab <b2ra830@gmail.com>
2025-09-05 22:44:20 +0530

    private access using setter and getter

---
+++ 10. OOPS/Encapsulation.java
@@ -0,0 +1,28 @@
+
+class Account {
+
+    public String name;
+    protected String email;
+    private String password;
+
+    public void setPass(String password) {
+        this.password = password;
+    }
+
+    public String getPass() {
+        return this.password;
+    }
+}
+
+public class Encapsulation {
+
+    public static void main(String[] args) {
+        Account a = new Account();
+        a.name = "jd";
+        a.email = "ab@jd.com";
+        // password is defind as private so wont be access so use setter andgetter
+        // a.password = "12378rywg"; 
+        a.setPass("53475frfw");
+        System.out.println(a.getPass());
+    }
+}
