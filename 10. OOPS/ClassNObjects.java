903cc6f56ead36acd0842f1a40a04611f24404db
bitturana-ab <b2ra830@gmail.com>
2025-09-05 22:08:18 +0530

    copy constructor

--- 10. OOPS/ClassNObjects.java
+++ 10. OOPS/ClassNObjects.java
@@ -8,10 +8,19 @@ class Mobile {
         System.out.println(name + " " + price);
     }
 
+    public Mobile() {
+    }
+
     public Mobile(String name, int price) {
         this.name = name;
         this.price = price;
         System.out.println(name + " " + price);
+    }
+
+    // copy constructor
+    Mobile(Mobile m) {
+        this.name = m.name;
+        this.price = m.price;
     }
 
 }
@@ -19,11 +28,18 @@ class Mobile {
 public class ClassNObjects {
 
     public static void main(String jd[]) {
-        // Mobile m = new Mobile();
+        Mobile m = new Mobile();
         // m.name = "Samsung a54";
         // m.price = 45000;
         // m.printInfo();
-        Mobile m = new Mobile("Samsung-a54", 45000);
+        // Mobile m1 = new Mobile("Samsung-a54", 45000);
+
+        // copy constructor
+        m.name = "samsung-a55";
+        m.price = 34000;
+        Mobile copyMobile = new Mobile(m);
+        System.out.println(copyMobile.name);
+        System.out.println(copyMobile.price);
 
     }
 }
