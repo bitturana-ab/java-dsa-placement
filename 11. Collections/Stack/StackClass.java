ba7732275d9fba6245ee768805086619ec8e68b9
bitturana-ab <b2ra830@gmail.com>
2025-09-14 21:38:22 +0530

    linkedlist in folder && stack using array

---
+++ 11. Collections/Stack/StackClass.java
@@ -0,0 +1,68 @@
+
+public class StackClass {
+
+    static class Node {
+
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            next = null;
+        }
+
+    }
+
+    public static Node head;
+
+    public static boolean isImpty() {
+        return head == null;
+    }
+
+    // add at the top of the stack
+    public static void push(int data) {
+        Node newNode = new Node(data);
+        if (isImpty()) {
+            head = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    // remove from top of the stack
+    public static int pop() {
+        if (isImpty()) {
+            System.out.println("Stack is empty || underflow");
+            return -1;
+        }
+        int pop = head.data;
+        head = head.next;
+        return pop;
+
+    }
+
+    // peek the top element of the stack
+    public static int peek() {
+        if (isImpty()) {
+            System.out.println("Stack is empty || underflow");
+            return -1;
+        }
+        return head.data;
+    }
+
+    public static void main(String[] args) {
+        // Stack s = new Stack();
+        push(28);
+        push(10);
+
+        push(25);
+        push(22);
+
+        while (!isImpty()) {
+            System.out.println(peek());
+            pop();
+        }
+    }
+
+}
